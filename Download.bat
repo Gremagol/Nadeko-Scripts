40d33934dee5b079dd8656eb0c91e0531686b9d4
Gremagol <xgremagolx@gmail.com>
2017-09-23 11:14:06 +0200

    Added 1.8 Branch for 1.8 users.

--- Download.bat
+++ Download.bat
@@ -5,7 +5,7 @@ CD /D "%root%"
 ECHO Deleting NadekoBot Folder...
 RMDIR "%root%NadekoBot\" /S /Q >nul 2>&1
 ECHO Downloading Nadekobot...
-git clone -b 1.9 https://github.com/Kwoth/NadekoBot
+git clone -b 1.8 https://github.com/Kwoth/NadekoBot
 ECHO Nadekobot downloaded.
 timeout /t 5
 CALL NadekoBot.bat
